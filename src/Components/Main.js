724a24fbb77d7ea6f4dbb9af31048222201f9564
Sebastien Laifa <seblaifa@hotmail.com>
2021-06-30 10:30:07 -0700

    just started

---
+++ src/Components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class Main extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Keiran & Ethan</h1> 
+            </div>
+        );
+    }
+}
+
+export default Main;
